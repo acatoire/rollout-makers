cdbb12dfa9d043a6ca96a5f962c70212914c5c2e
cmpprevot <50751821+cmpprevot@users.noreply.github.com>
2019-09-10 14:17:21 +0200

    Add files via upload
    connect my travel to Ook_screen

--- sketches/OoK_Screen/OoK_Screen.ino
+++ sketches/OoK_Screen/OoK_Screen.ino
@@ -6,11 +6,12 @@
 #define BLYNK_PRINT Serial
 
 #include <WiFi.h>
-#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
 #include <ESPmDNS.h>
 
 #include <BlynkSimpleEsp32.h>
 #include <MyScreen.h>
+#include <MyTravel.h>
 
 #include "config.h"
 
@@ -21,45 +22,45 @@
 #define SCREEN_MAX    V13
 #define SCREEN_TEXT   V14
 
-uint8_t screen_id      = 0;
+uint8_t screen_id      = 3;
 uint8_t screen_option   = 0;
 uint8_t screen_actual  = 0;
 uint8_t screen_max     = 0;
 uint8_t* screen_text    = (uint8_t*)"1234567890ABCDEFGHIJ";
 
 // IDs definition
-enum ScreenId 
-{ 
-  SCREEN_WELCOME     = 0, 
-  SCREEN_TESTBENCH   = 1, 
-  SCREEN_JENKINS     = 2, 
+enum ScreenId
+{
+  SCREEN_WELCOME     = 0,
+  SCREEN_TESTBENCH   = 1,
+  SCREEN_JENKINS     = 2,
   SCREEN_MOBILITY    = 3,
   SCREEN_BATTERY     = 4,
   SCREEN_PICT        = 5
 };
 
-enum MobilityId 
-{ 
-  MOB_WALK   = 1, 
-  MOB_BIKE   = 2, 
+enum MobilityId
+{
+  MOB_WALK   = 1,
+  MOB_BIKE   = 2,
   MOB_TRAM   = 3,
-  MOB_CAR    = 4 
+  MOB_CAR    = 4
 };
 
-enum PictureId 
-{ 
-  PICT_NO            = 0, 
-  PICT_WALKING_MAN   = 1, 
-  PICT_BIKE          = 2, 
+enum PictureId
+{
+  PICT_NO            = 0,
+  PICT_WALKING_MAN   = 1,
+  PICT_BIKE          = 2,
   PICT_CAR           = 3,
-  PICT_TRAIN         = 4 
+  PICT_TRAIN         = 4
 };
 
-enum TestBenchScreenId 
-{ 
-  TEST_OFF     = 0, 
-  TEST_RUN     = 1, 
-  TEST_FAILED  = 2, 
+enum TestBenchScreenId
+{
+  TEST_OFF     = 0,
+  TEST_RUN     = 1,
+  TEST_FAILED  = 2,
   TEST_SUCCESS = 3
 };
 
@@ -68,6 +69,10 @@ enum TestBenchScreenId
 const char wifiPassword[] = WIFI_PASS;
 const char wifiSsid[] = WIFI_SSID;
 char blynkAuth[] = "xbzggg5NU6Ye9G4XD6utHK6l_3nqb5Lr";
+
+WiFiClientSecure client;
+String key = GOOGLE_MAP_API_KEY;
+MyTravel travel(client, key);
 
 bool newValue = false;
 uint16_t loopId = 0;
@@ -89,7 +94,7 @@ BLYNK_WRITE(SCREEN_ID)
 
 BLYNK_WRITE(SCREEN_OPTION)
 {
-  screen_option = param.asInt(); 
+  screen_option = param.asInt();
   newValue = true;
   Serial.print("screen_option value is: ");
   Serial.println(screen_option);
@@ -97,7 +102,7 @@ BLYNK_WRITE(SCREEN_OPTION)
 
 BLYNK_WRITE(SCREEN_ACTUAL)
 {
-  screen_actual = param.asInt(); 
+  screen_actual = param.asInt();
   newValue = true;
   Serial.print("screen_actual value is: ");
   Serial.println(screen_actual);
@@ -105,7 +110,7 @@ BLYNK_WRITE(SCREEN_ACTUAL)
 
 BLYNK_WRITE(SCREEN_MAX)
 {
-  screen_max = param.asInt(); 
+  screen_max = param.asInt();
   newValue = true;
   Serial.print("screen_max value is: ");
   Serial.println(screen_max);
@@ -113,14 +118,14 @@ BLYNK_WRITE(SCREEN_MAX)
 
 BLYNK_WRITE(SCREEN_TEXT)
 {
-  screen_text = (uint8_t*)param.asString(); 
+  screen_text = (uint8_t*)param.asString();
   newValue = true;
   Serial.print("screen_text value is: ");
   Serial.println((char*)screen_text);
 }
 
 // Setup
-void setup(void) 
+void setup(void)
 {
   Serial.begin(115200);
   Serial.print("Setup Start\n");
@@ -146,16 +151,17 @@ void valueUpdate(void)
 // Function to periodically update the screen
 void screenUpdate(void)
 {
-
+  DirectionsResponse infoBike;
+  DirectionsResponse infoCar;
   screen.Clear();
-    
+
   switch (screen_id)
   {
     case SCREEN_WELCOME :
 
-        //screen.PrintFull(WHITE);
-        //screen.PrintBmp(1, RED, 0, 0);
-        screen.PrintStickMan(loopId, GREEN, loopId, 0);
+      //screen.PrintFull(WHITE);
+      //screen.PrintBmp(1, RED, 0, 0);
+      screen.PrintStickMan(loopId, GREEN, loopId, 0);
 
       if (32 == loopId)
       {
@@ -165,72 +171,73 @@ void screenUpdate(void)
       {
         loopId++;
       }
-      
 
-    break;
+
+      break;
 
     case SCREEN_TESTBENCH :
       switch (screen_id)
       {
         case TEST_RUN :
           screen.PrintTestStatus(screen_actual, screen_max);
-        break;
+          break;
 
         case TEST_FAILED :
           screen.PrintOoK(OoK_Fail);
-        break;
+          break;
 
         case TEST_SUCCESS :
           screen.PrintOoK(OoK_Success);
-        break;
-        
+          break;
+
         case TEST_OFF :
         default:
           screen.Clear();
-        break;
+          break;
       }
-    break;
+      break;
 
     case SCREEN_JENKINS :
       screen.PrintJenkins(screen_text);
-    break;
+      break;
 
     case SCREEN_MOBILITY :
-          if (loopId < 50)
-          {
-              screen.PrintTwoTimes(25,35);
-          }
-          else if (loopId < 75)
-          {
-              screen.PrintBike(GREEN, 0, 0);
-          }
-          else if (loopId < 100)
-          {
-              screen.PrintCar(RED, 0, 0);
-          }
-          
-          loopId++;
-          loopId = loopId % 100;
-    break;
+      infoBike = travel.GetInfoByBike();
+      infoCar = travel.GetInfoByCar();
+      if (loopId < 50)
+      {
+        printf("test");
+        screen.PrintTwoTimes(infoBike.duration_value, infoCar.duration_value);
+      }
+      else if (loopId < 75)
+      {
+        screen.PrintBike(GREEN, 0, 0);
+      }
+      else if (loopId < 100)
+      {
+        screen.PrintCar(RED, 0, 0);
+      }
+
+      loopId++;
+      loopId = loopId % 100;
+      break;
 
     case SCREEN_BATTERY :
-          screen.PrintBattery(screen_actual * 10, WHITE, 0, 0);
-    break;
+      screen.PrintBattery(screen_actual * 10, WHITE, 0, 0);
+      break;
 
     case SCREEN_PICT :
-          screen.PrintBmp(screen_option, WHITE, RED, 0);
-    break;
+      screen.PrintBmp(screen_option, WHITE, RED, 0);
+      break;
   }
 
   screen.Update();
 }
 
 
-void loop(void) 
+void loop(void)
 {
   Blynk.run();
   timerScreenUpdate.run();
   timerValueUpdate.run();
 }
-
-
